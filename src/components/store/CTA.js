7cf95f1cdf79e0d833cc426d4c0a131ac89ca38f
Isabel <isabel.asenjo@correo.unimet.edu.ve>
2022-01-17 17:03:55 -0400

    awesome-reveal @ store page

--- src/components/store/CTA.js
+++ src/components/store/CTA.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './CTA.css';
 
 export default function CTA() {
     return (
         <div className="store-cta-container">
             <h2>¡Pide cualquiera de estos productos rellenando el siguiente Form!</h2>
-            <a className="store-cta-button" href="https://www.google.com/forms/about/" target="_blank">Ir al Form</a>
+            <motion.a className="store-cta-button" href="https://www.google.com/forms/about/" target="_blank">Ir al Form</motion.a>
         </div>
     )
 }
--- src/components/store/Store.js
+++ src/components/store/Store.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Fade } from "react-awesome-reveal";
 import './Store.css';
 import Header from '../common/Header';
 import Text from './Text';
@@ -8,10 +9,18 @@ import CTA from './CTA';
 export default function Store({ products = [] }) {
     return(
       <div className="">
-        <Header image="/images/saman.jpg" title="TIENDA" text="" />
-        <Text />
-        <Carousel products={products} />
-        <CTA />
+        <Fade cascade triggerOnce>
+            <Header image="/images/saman.jpg" title="TIENDA" text="" />
+        </Fade>
+        <Fade cascade triggerOnce>
+            <Text />
+        </Fade>
+        <Fade cascade triggerOnce>
+            <Carousel products={products} />
+        </Fade>
+        <Fade cascade triggerOnce>
+            <CTA />
+        </Fade>
       </div>
     )
 }
