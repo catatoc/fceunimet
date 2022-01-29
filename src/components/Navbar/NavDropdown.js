f391050ee8ade09d002de7c9e88a2b8800b10aa9
Maximiliano Pérez Tambasco <maxperez243@gmail.com>
2022-01-29 11:31:26 +0100

    Navbar incorporado el boton movil de Menu, boton Mas es ahora mas responsive
    - Para pantallas moviles aparecera el boton Menú y desaparecern todos los demás items del Navbar
    -El botón Más ahora va incorporando items al dropdown a medida que se va achicando la pantalla hasta llegar a tamaño de pantalla movil donde desaparecera

--- src/components/Navbar/NavDropdown.js
+++ src/components/Navbar/NavDropdown.js
@@ -3,15 +3,25 @@ import { Link } from "react-router-dom";
 import "./NavDropdown.css";
 
 // Los estilos de clases nav-links estan definidos en Navbar.css:
-export const MasDropdown1024 = () => {
+export const MasDropdown = () => {
   return (
     <>
-      <button className="dropbtn">
+      <button className="dropbtn mas-dropdown">
         <p>
           Más <span>↓</span>
         </p>
       </button>
       <ul className="dropdown-content">
+        <li>
+          <Link to="/" className="nav-links">
+            Calendario
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">
+            Tienda
+          </Link>
+        </li>
         <li>
           <Link to="/" className="dropdown-links">
             Proyectos
@@ -19,6 +29,60 @@ export const MasDropdown1024 = () => {
         </li>
         <li>
           <Link to="/" className="dropdown-links">
+            Ayuda
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export const MenuDropdown = () => {
+  return (
+    <>
+      <button className="dropbtn menu-dropdown">
+        <p>
+          Menú <span>↓</span>
+        </p>
+      </button>
+      <ul className="dropdown-content">
+        <li>
+          <Link to="/" className="nav-links">
+            Nosotros
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">
+            Escuelas
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">
+            Agrupaciones
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">
+            Universidad
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">
+            Calendario
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">
+            Tienda
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">
+            Proyectos
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">
             Ayuda
           </Link>
         </li>
