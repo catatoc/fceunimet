7293bde6c16344ea626244ae02e291f947534161
Sabrina Correia <sabrina.correia@correo.unimet.edu.ve>
2022-02-01 17:52:44 -0400

    el video de home ya no se ve en pantalla de telefono y ahora el primer video es mas rapido y el componente se comunica directo con app.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-      <video src='/videos/drone.mp4' autoPlay loop muted />
+    <div className="hero-container">
+      <video src="/videos/drone.mp4" autoPlay loop muted />
+      <img className="phone-bg" src="/images/unimet3.jpg"></img>
       <h1>FCE UNIMET</h1>
       <p>Estudiantes comprometidos por el bienestar unimetano</p>
-      <div className='hero-btns'>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
         >
           CONTACTO
         </Button>
         <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={console.log("hey")}
         >
-          ENTÉRATE <i className='far fa-play-circle' />
+          ENTÉRATE <i className="far fa-play-circle" />
         </Button>
       </div>
     </div>
--- src/components/VideoFCE.js
+++ src/components/VideoFCE.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './VideoFCE.css';
+import React from "react";
+import { useRef, useState } from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./VideoFCE.css";
 import { Fade } from "react-awesome-reveal";
 
 // Video que saldrá al iniciar la página y cuando esté cargando.
 
-function VideoFCE() {
+function VideoFCE({ childToParent }) {
+  let firstVideoEnded = true;
+
+  const videoRef = useRef();
+  const setPlayBack = () => {
+    videoRef.current.playbackRate = 2;
+  };
+
   return (
     <Fade cascade triggerOnce>
-      <div className='hero-containerFCE'>
-        <video src='/videos/FCEUNIMET.mp4' autoPlay muted />
+      <div className="hero-containerFCE">
+        <video
+          src="/videos/FCEUNIMET.mp4"
+          autoPlay
+          muted
+          ref={videoRef}
+          onCanPlay={() => setPlayBack()}
+          onEnded={() => childToParent(firstVideoEnded)}
+        />
       </div>
     </Fade>
   );
