ff2311b02eb2209cf3496976ca3915e0a9d16501
catatoc <ccarrasquero@correo.unimet.edu.ve>
2022-02-28 11:03:12 -0400

    social network en nosotros (puede verse mejor)

--- src/components/us/Us.js
+++ src/components/us/Us.js
@@ -5,6 +5,11 @@ import Text from '../common/Text'; //Esto hay que cambiarlo cuando se mueva de s
 import CA from './CA'
 import JD from './JD'
 import JoinUs from './JoinUs';
+import WhatsAppIcon from './../../components/svg/whatsapp-white.svg'
+import InstagramIcon from './../../components/svg/instagram-blanco.svg'
+import YoutubeIcon from './../../components/svg/youtube-blanco.svg'
+import '../../App.css';
+import './../FAQs/cards/question.css';
 
 export default function Us() {
     return(
@@ -12,6 +17,31 @@ export default function Us() {
         <Fade cascade triggerOnce>
             <Header image="/images/presidentes-min-min.jpg" title="NOSOTROS" text="" />
             <Text text={'¡Un equipo de 104 personas comprometidas con el bienestar unimetano!'} />
+            <div className='btn-whatsapp'>
+              <a className='anchor-whatsapp' href="https://api.whatsapp.com/send?phone=584241961948" target="_blank">
+                <img src={WhatsAppIcon} alt='WhatsApp'></img>
+              </a>
+            </div>
+            {/* Esto de social network lo codee todo en App.css por si acaso queremos usarlo en otro lado. Puede verse mejor pero mientras tanto.*/}
+            <div className='social-network'> 
+              <div className='btn-whatsapp-sm'>
+                <a className='anchor-whatsapp' href="https://api.whatsapp.com/send?phone=584241961948" target="_blank">
+                  <img src={WhatsAppIcon} alt='WhatsApp'></img>
+                </a>
+              </div>
+              <div className='btn-instagram'>
+                <a className='anchor-whatsapp' href="https://api.whatsapp.com/send?phone=584241961948" target="_blank">
+                  <img src={InstagramIcon} alt='WhatsApp'></img>
+                </a>
+              </div>
+              <div className='btn-youtube'>
+                <a className='anchor-whatsapp' href="https://api.whatsapp.com/send?phone=584241961948" target="_blank">
+                  <img src={YoutubeIcon} alt='WhatsApp'></img>
+                </a>
+              </div>
+
+            </div>
+
             <CA />
             <JD />
             <JoinUs />
