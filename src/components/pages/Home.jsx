26bf313a4a75de1114a555d212d6deb502726fa9
catatoc <ccarrasquero@correo.unimet.edu.ve>
2022-02-28 00:20:50 -0400

    pruebas. No debi hacerlo en develop

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,6 +7,8 @@ import FloatingWhatsApp from 'react-floating-whatsapp';
 import logo from './../../logo_blanco.png';
 import Footer from "../footer/Footer";
 import WhatsAppIcon from './../../components/svg/whatsapp-white.svg'
+import InstagramIcon from './../../components/svg/instagram-blanco.svg'
+import YoutubeIcon from './../../components/svg/youtube-blanco.svg'
 
 function Home() {
   return (
@@ -16,6 +18,16 @@ function Home() {
         <div className='btn-whatsapp'>
           <a className='anchor-whatsapp' href="https://api.whatsapp.com/send?phone=584241961948" target="_blank">
 		        <img src={WhatsAppIcon} alt='WhatsApp'></img>
+	        </a>
+        </div>
+        <div className='btn-instagram'>
+          <a className='anchor-whatsapp' href="https://api.whatsapp.com/send?phone=584241961948" target="_blank">
+		        <img src={InstagramIcon} alt='WhatsApp'></img>
+	        </a>
+        </div>
+        <div className='btn-youtube'>
+          <a className='anchor-whatsapp' href="https://api.whatsapp.com/send?phone=584241961948" target="_blank">
+		        <img src={YoutubeIcon} alt='WhatsApp'></img>
 	        </a>
         </div>
     </>
