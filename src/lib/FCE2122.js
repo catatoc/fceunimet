c7e65d95c4f4d0abcc7329b8de367932ba2b9243
catatoc <ccarrasquero@correo.unimet.edu.ve>
2022-02-28 10:40:23 -0400

    faster image load

--- src/lib/FCE2122.js
+++ src/lib/FCE2122.js
@@ -3,47 +3,47 @@ export const jd = [
         position: 'Secretaria General',
         name: 'Raquel Greige',
         info: 'Estudiante de Derecho',
-        src: '/images/raquel.jpg',
+        src: '/images/raquel-min.jpg',
         alt: 'Imagen del Secretario General',
     },
     {
         position: 'Presidente',
         name: 'Ricardo Pulido',
         info: 'Estudiante de Ciencias Administrativas, mención Gerencia y mención Banca y Finanzas',
-        src: '/images/pulido.jpg',
+        src: '/images/pulido-min.jpg',
         alt: 'Imagen del Presidente',
     },
     {
         position: 'Secretario de Asuntos Internos',
         name: 'Carlos Carrasquero',
         info: 'Ingeniero de Producción y estudiante de Ingeniería de Sistemas',
-        src: '/images/catato.jpg',
+        src: '/images/catato-min.jpg',
         alt: 'Imagen del Secretario de Asuntos Internos',
     },
     {
         position: 'Coordinador General',
         name: 'Marcello Piunno',
         info: 'Estudiante de Ciencias Administrativas, mención Gerencia',
-        src: '/images/marcello.jpg',
+        src: '/images/marcello-min.jpg',
         alt: 'Imagen del Coordinador General',
     },
     {
         position: 'Tesorero',
         name: 'Santiago Pérez Mussa',
         info: 'Estudiante de Economía Empresarial',
-        src: '/images/santiago.jpg',
+        src: '/images/santiago-min.jpg',
         alt: 'Imagen del Tesorero',
     }
 ]
 
 export const ca = [
     {
-        src: '/images/nicolle.png',
+        src: '/images/nicolle-min.png',
         name: 'Nicolle Giraud',
         info: 'Estudiante de Ingeniería Química',
     },
     {
-        src: '/images/jose.png',
+        src: '/images/jose-min.png',
         name: 'José Cisneros',
         info: 'Estudiante de Ingeniería de Sistemas y Producción',
     }
